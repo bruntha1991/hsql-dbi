f6203d3b14bfc60e97d9cc4571d6c5f71698e8af
Ruba <ruba.11@cse.mrt.ac.lk>
2016-02-07 18:39:41 +0530

    Stop word ignore added

--- src/org/hsqldb/util/DatabaseManager.java
+++ src/org/hsqldb/util/DatabaseManager.java
@@ -40,6 +40,9 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
 import java.awt.BorderLayout;
 import java.awt.Button;
@@ -857,10 +860,29 @@ implements ActionListener, WindowListener, KeyListener {
 
         if(sCmd.contains("FREE_TEXT")){
             String[] commandArray=sCmd.split("\\(\"");
-            String sql=commandArray[1].substring(0,commandArray[1].length()-2);
-            System.out.println(sql);
+            String sql=commandArray[1].substring(0, commandArray[1].length() - 2);
+            List<String> stopWords = new ArrayList<>(Arrays.asList("a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"));
+
             TfIdfMain tfIdfMain=new TfIdfMain();
             try {
+                sql = sql.replaceAll("[^a-zA-Z]","").toLowerCase().trim();
+                if(sql.length() == 0)
+                    return;
+
+                //starting from here
+                String words[] = sql.split(" ");
+                sql = "";
+                for(String word: words){
+                    if(word.length() == 0)
+                        continue;
+                    else if(stopWords.contains(word))
+                        continue;
+                    else
+                        sql+=" "+word;
+                }
+                sql = sql.trim();
+                //to here it ignores stop words Prakhash
+
                 String[][] dataForUpdate = tfIdfMain.ProcessQuery(sql);
                 String[] head={"ID","FIRSTNAME","LASTNAME","STREET","CITY"};
                 gResult.setHead(head);
